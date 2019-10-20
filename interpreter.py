81d05872db548a816cd46be99a0956f65484dbe8
Kevin Gomez <kgscience@hotmail.com>
2019-10-20 11:37:16 -0500

    Added human-readable verbosity

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,11 @@
 # Nonsense interpreter
-import sys
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description="Run Nonsense programs.")
+parser.add_argument("paragraph", type=str, help="A Nonsense paragraph.")
+parser.add_argument("--verbose", "-v", action="store_true")
+args = parser.parse_args()
 
 VOWELS = " aeiouy"
 CONSONANTS = "0bcdfghjklmnpqrstvwxz"
@@ -18,11 +23,6 @@ def load_words():
     return valid_words
 
 
-def check_word(word):
-    english_words = load_words()
-    return word in english_words
-
-
 def extend(index):
     S.extend([0] * (index - len(S) + 1))
 
@@ -33,12 +33,19 @@ def execute(syllable):
     add = 0
     inc = False
 
+    init_str = init
+    instr_str = ""
+    sec_str = ""
+    loc_str = ""
+    print_str = ""
+
     index = 1
     letter = syllable[1]
     while letter in VOWELS:
         if letter == "a":
             add = 1
         elif letter == "e":
+            init_str = "S[" + init_str + "]"
             if op >= len(S):
                 op = 0
             else:
@@ -52,25 +59,64 @@ def execute(syllable):
             if op == 0 and syllable[0] == "0":
                 op = int(input(": "))
             else:
-                print(chr(op), end="")
+                print_str += chr(op)
 
         index += 1
         letter = syllable[index]
 
     loc = letter
-    if len(syllable) > index + 1 and syllable[index+1] == "e":
+    if add > 0:
+        instr_str = "+"
+    elif add < 0:
+        instr_str = "-"
+    elif inc:
+        instr_str = "+"
+
+    if len(syllable) > index + 2 and syllable[index + 2] == "e":
+        extend(S[VALS[loc]])
+        S[0] = op + S[S[VALS[loc]]] * add
+        if inc:
+            sec_str = str(add + (add == 0))
+        elif add != 0:
+            sec_str = "S[S[" + loc + "]]"
+
+        if loc != "0":
+            S[S[VALS[loc]]] = S[0]
+            loc_str = "S[S[" + loc + "]]"
+        elif inc:
+            S[S[VALS[init]]] = S[0]
+            loc_str = "S[S[" + init + "]]"
+    elif len(syllable) > index + 1 and syllable[index + 1] == "e":
         extend(VALS[loc])
         S[0] = op + S[VALS[loc]] * add
+        if inc:
+            sec_str = str(add + (add == 0))
+        elif add != 0:
+            sec_str = "S[" + loc + "]"
+
         if loc != "0":
             S[VALS[loc]] = S[0]
+            loc_str = "S[" + loc + "]"
         elif inc:
             S[VALS[init]] = S[0]
+            loc_str = "S[" + init + "]"
     else:
         S[0] = op + VALS[loc] * add
+        if inc:
+            sec_str = str(add + (add == 0))
+        elif add != 0:
+            sec_str = loc
+
         if loc != "0":
             VALS[loc] = S[0]
+            loc_str = loc
         elif inc:
             VALS[init] = S[0]
+            loc_str = init
+
+    if args.verbose:
+        print(syllable + ": " + init_str + instr_str + sec_str + ("->" if loc_str else "") + loc_str)
+    return print_str
 
 
 def search(words, counter):
@@ -85,7 +131,7 @@ def search(words, counter):
 
 
 def main():
-    paragraph = sys.argv[1].lower()
+    paragraph = args.paragraph.lower()
     if "\\" in paragraph:
         paragraph = paragraph[:paragraph.find("\\")]
 
@@ -93,6 +139,7 @@ def main():
         paragraph = paragraph.replace(punc, " " + punc)
     words = paragraph.split()
     syllables = []
+    print_str = ""
 
     english_words = load_words()
     for word in words:
@@ -114,14 +161,16 @@ def main():
                 while index < len(word) and word[index] in VOWELS:
                     index += 1
                 if index < len(word):
-                    syllables.append(word[start:index + 1 + (index + 1 < len(word) and word[index+1] == "e")])
+                    syllables.append(word[start:index + 1 + (index + 1 < len(word) and word[index + 1] == "e") + (
+                                index + 2 < len(word) and word[index + 2] == "e")])
 
         elif word.isnumeric():
             syllables.append(int(word))
         elif word not in PUNCTUATION:
             raise ValueError(word + " is not a numeral or English word.") from None
 
-    print("Syllables: " + str(syllables))
+    if args.verbose:
+        print("Syllables: ", str(syllables))
 
     counter = 0
     while counter < len(syllables):
@@ -146,13 +195,17 @@ def main():
                 elif seg == "?":
                     counter += 1 + (not S[0])
             else:
-                execute(seg)
+                print_str += execute(seg)
                 counter += 1
         elif isinstance(seg, int):
             S[0] = seg
             counter += 1
         else:
             raise ValueError("Invalid word passed through compilation.") from None
+
+    if args.verbose:
+        print("Output: ")
+    print(print_str)
 
 
 if __name__ == "__main__":
